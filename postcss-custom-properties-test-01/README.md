81f8432c2edf712db6dd7bbc0e4f0c3a98e7a792
Jonathan Neal <jonathantneal@hotmail.com>
2018-06-05 15:09:48 -0400

    Add PostCSS Custom Properties test

---
+++ postcss-custom-properties-test-01/README.md
@@ -0,0 +1,3 @@
+# PostCSS Custom Properties Test
+
+This demonstrates how comments are handled by [postcss-custom-properties#86](https://github.com/postcss/postcss-custom-properties/issues/86).
---
+++ postcss-custom-properties-test-01/test.js
@@ -0,0 +1,37 @@
+const fs      = require('fse');
+const postcss = require('postcss');
+const plugin  = require('postcss-custom-properties');
+
+// symbols
+const isWin32 = process.platform === 'win32';
+const tick    = isWin32 ? '√' : '✔';
+const cross   = isWin32 ? '×' : '✖';
+
+// test name
+const testname = 'PostCSS Custom Properties (Issue #120)';
+
+const sourceFile = 'test.css';
+const expectFile = 'test.expect.css';
+const resultFile = 'test.result.css';
+
+fs.readFile(sourceFile, 'utf8').then(
+	sourceCSS => postcss([
+		plugin({ preserve: 'computed' })
+	]).process(sourceCSS, { to: resultFile, from: sourceFile })
+).then(
+	result => Promise.all([
+		fs.writeFile(resultFile, result.css),
+		fs.readFile(expectFile, 'utf8')
+	]).then(
+		results => result.css === results[1] ? true : Promise.reject('results differed')
+	)
+).then(
+	pass => {
+		console.log(tick, testname, 'test passed');
+		process.exit(0);
+	},
+	error => {
+		console.log(cross, testname, 'test failed', error);
+		process.exit(1);
+	}
+);
